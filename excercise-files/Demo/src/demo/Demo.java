bec6bc0232ab53129c5606b3edff328e69b0821d
jlilly <jlilly@users.noreply.github.com>
2017-12-17 09:40:38 -0500

    Demo
    write, compile, run

--- excercise-files/Demo/src/demo/Demo.java
+++ excercise-files/Demo/src/demo/Demo.java
@@ -1,5 +1,5 @@
 /*
- * Demo
+ * Demo: write, compile, run
  */
 package demo;
 
@@ -13,7 +13,12 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Java is case-sensitive
+        // Double quotes around letters or numbers is considered a literal in Java.
+        System.out.println("Hello, and welcome to up and running with Java");
+        // Building the project creates a byte code file, 
+        // where the IDE converts the program to an object file
+        // that can be understood by the JVM
     }
     
 }

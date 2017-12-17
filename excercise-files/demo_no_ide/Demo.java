19e1d6d812d1a3847268ed038cbfc1ed14fdf923
jlilly <jlilly@users.noreply.github.com>
2017-12-17 10:03:25 -0500

    Demo
    Write, compile, and run without an IDE.

---
+++ excercise-files/demo_no_ide/Demo.java
@@ -0,0 +1,36 @@
+// create Java program using a text editor instead of an IDE
+
+// class files always start with a capital letter
+// files must be named the same as the class name
+public class Demo {
+	// args is the name of our string variable
+	public static void main (String[] args) {
+		System.out.println("Hello, and welcome to up and running with Java");
+	}
+}
+
+// compile using the command prompt
+// set path according to the version of the JDK you have downloaded
+/*
+
+	set path=%path%;c:\Program Files\java\jdk1.7.0_80\bin
+
+*/
+// change directory to where the file is
+/*
+
+	cd myJava
+
+*/
+// compile with an executable
+/*
+
+	javac Demo.java
+
+*/
+// look at the files that have been created
+/*
+
+	dir
+
+*/

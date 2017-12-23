943248595ee84b29f3f405f62a286c89d7ba692a
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2017-12-23 15:13:25 -0500

    Understanding data types

--- excercise-files/02_02/TravelCost/src/travelcost/TravelCost.java
+++ excercise-files/02_02/TravelCost/src/travelcost/TravelCost.java
@@ -1,13 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package travelcost;
+import java.util.Scanner;
 
 /**
  *
  * @author Peggy Fisher
+ * @editor jlilly
+ * 
+ * @notes
+ * Understanding Data Types
+ * Java is known as a strict data typing language. That means that every variable that we define must be declared with a data type. And you can not change the type of data stored in that variable.
+ * 
+ * Primitive Data Types
+ * Characters
+ *      char LetterA = 'a';
+ * Boolean
+ *      boolean result = true;
+ * Integers (no fractional part)
+ *      byte // signed values from -127 to +127, unsigned values fro 0 to 255
+ *      short // 
+ *      int
+ *      long
+ * Rational Numbers (may have a fractional part)
+ *      float
+ *      double
  */
 public class TravelCost {
 
@@ -15,7 +30,23 @@ public class TravelCost {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double distance;
+        double mpg;
+        double pricePerGallon;
+        double totalCost;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the total distance in miles: ");
+        distance = in.nextDouble();
+        System.out.println("Enter the mpg for the vehicle: ");
+        mpg = in.nextDouble();
+        System.out.println("Enter the price of one gallon of gas: ");
+        pricePerGallon = in.nextDouble();
+        
+        totalCost = (distance/mpg)*pricePerGallon;
+        System.out.printf("The trip is going to cost $%5.2f", totalCost);
+        System.out.println();
+        
+        // build, run
+        
     }
-    
 }
